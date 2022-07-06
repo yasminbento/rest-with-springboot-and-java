7cb547f44a175f8d05b34e4d74607d6ecf55077e
Yasmin Bento <yasmoongusgoz@gmail.com>
2022-07-06 19:41:03 -0300

    inclusao das demais operações matematicas

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -20,6 +20,72 @@ public class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double subtraction (
+            @PathVariable(value = "numberOne")   String numberOne,
+            @PathVariable(value = "numberTwo")   String numberTwo
+    ) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please, set a numeric value");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+       @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multiplication (
+            @PathVariable(value = "numberOne")   String numberOne,
+            @PathVariable(value = "numberTwo")   String numberTwo
+    ) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please, set a numeric value");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/divide/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double divide (
+            @PathVariable(value = "numberOne")   String numberOne,
+            @PathVariable(value = "numberTwo")   String numberTwo
+    ) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please, set a numeric value");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/media/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double media (
+            @PathVariable(value = "numberOne")   String numberOne,
+            @PathVariable(value = "numberTwo")   String numberTwo
+    ) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please, set a numeric value");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+
+    @RequestMapping(value = "/square/{number}", method = RequestMethod.GET)
+    public Double square (
+            @PathVariable(value = "number")   String number
+
+    ) throws Exception{
+
+        if (!isNumeric(number)) {
+            throw new UnsupportedMathOperationException("Please, set a numeric value");
+        }
+        return Math.sqrt(convertToDouble(number));
+    }
+
+
+
+
+
+
+
     private Double convertToDouble(String strNumber) {
         if (strNumber == null) return 0D;
         String number = strNumber.replaceAll(",", ".");
